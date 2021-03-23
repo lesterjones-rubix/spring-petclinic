980743031fe31ca3758a7ccd43e9ca685532fa4a
Lester Jones <lester.jones@rubix.nl>
2021-03-23 10:45:35 +0100

    changed warthog to snake

--- src/main/resources/db/h2/data.sql
+++ src/main/resources/db/h2/data.sql
@@ -18,7 +18,7 @@ INSERT INTO vet_specialties VALUES (5, 1);
 INSERT INTO types VALUES (1, 'cat');
 INSERT INTO types VALUES (2, 'dog');
 INSERT INTO types VALUES (3, 'sea horse');
-INSERT INTO types VALUES (4, 'warthog');
+INSERT INTO types VALUES (4, 'snake');
 INSERT INTO types VALUES (5, 'eagle');
 INSERT INTO types VALUES (6, 'horse');
 
